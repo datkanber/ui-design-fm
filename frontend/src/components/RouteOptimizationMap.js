05fba8eb51fef3e9c9c88bedecc7420122ae4d30
Pilestin <yasin.unal@bil.omu.edu.tr>
2025-02-28 01:44:26 +0300

    v1.4:
    * Map Layers and esri satellite views are added

--- frontend/src/components/RouteOptimizationMap.js
+++ frontend/src/components/RouteOptimizationMap.js
@@ -16,8 +16,82 @@ export default function RouteOptimizationMap({ vehicles, chargingStations, route
   return (
     <MapContainer center={[39.750745, 30.482254]} zoom={16} style={{ height: `${height}px`, borderRadius: '12px', overflow: 'hidden' }}>
       <LayersControl position="topright">
-        <LayersControl.BaseLayer checked name="OpenStreetMap">
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* OpenStreetMap - Standart */}
+        <LayersControl.BaseLayer checked name="OpenStreetMap Standart">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </LayersControl.BaseLayer>
+
+        {/* OpenStreetMap - Humanitarian */}
+        <LayersControl.BaseLayer name="OpenStreetMap Humanitarian">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </LayersControl.BaseLayer>
+
+        {/* Google Maps Stil */}
+        <LayersControl.BaseLayer name="Google Streets">
+          <TileLayer
+            url="https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+            maxZoom={20}
+            subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+            attribution='&copy; Google Maps'
+          />
+        </LayersControl.BaseLayer>
+
+        {/* Google Satellite */}
+        <LayersControl.BaseLayer name="Google Satellite">
+          <TileLayer
+            url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+            maxZoom={20}
+            subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+            attribution='&copy; Google Maps'
+          />
+        </LayersControl.BaseLayer>
+
+        {/* Google Terrain */}
+        <LayersControl.BaseLayer name="Google Terrain">
+          <TileLayer
+            url="https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}"
+            maxZoom={20}
+            subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+            attribution='&copy; Google Maps'
+          />
+        </LayersControl.BaseLayer>
+
+        {/* Esri WorldStreetMap */}
+        <LayersControl.BaseLayer name="Esri WorldStreetMap">
+          <TileLayer
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+            attribution='&copy; Esri &mdash; Sources: Esri, HERE, Garmin, USGS, Intermap, INCREMENT P, NRCan, Esri Japan, METI, Esri China (Hong Kong), Esri Korea, Esri (Thailand), NGCC, (c) OpenStreetMap contributors, and the GIS User Community'
+          />
+        </LayersControl.BaseLayer>
+
+        {/* Esri Satellite */}
+        <LayersControl.BaseLayer name="Esri Satellite">
+          <TileLayer
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            attribution='&copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+          />
+        </LayersControl.BaseLayer>
+
+        {/* CartoDB - Aydınlık */}
+        <LayersControl.BaseLayer name="CartoDB Aydınlık">
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          />
+        </LayersControl.BaseLayer>
+
+        {/* CartoDB - Karanlık */}
+        <LayersControl.BaseLayer name="CartoDB Karanlık">
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          />
         </LayersControl.BaseLayer>
 
         <LayersControl.Overlay name="Araçlar">
